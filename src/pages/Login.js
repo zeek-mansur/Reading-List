2035f1baef59b767228fdd48f76d5af8015c1d0d
Zeek <mzulkiful@yahoo.com>
2022-10-01 16:38:10 +0100

    Logging Users In

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,14 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const {error, login} = useLogin()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password)
-
-    setEmail('')
-    setPassword('')
+    login(email, password)
   }
   
   return (
@@ -35,6 +34,7 @@ export default function Login() {
           />
         </label>
         <button>log in</button>
+        {error && <p>{error}</p>}
       </form>
     </div>
   )
