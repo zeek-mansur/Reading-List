39cadd32e4eabb26379fe31c98aa1403262370b0
Zeek <mzulkiful@yahoo.com>
2022-10-01 15:44:32 +0100

    Real-Time Data (Custom Hook)

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,28 @@
+import { db } from '../firebase/config'
+import { useEffect, useState } from 'react'
+
+// firebase import 
+import { collection, onSnapshot } from 'firebase/firestore'
+
+export const useCollection = (c) => {
+
+    const [documents, setDocuments] = useState(null)
+
+    useEffect(() => {
+                let ref = collection(db, c)
+
+              const unsub = onSnapshot(ref, (snapshot) => {
+                let results = []
+                snapshot.docs.forEach(doc => {
+                    results.push({...doc.data(), id: doc.id})
+                })
+
+                setDocuments(results)
+                })
+
+                return () => unsub()
+
+    }, [c])
+    return {documents}
+}
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,12 @@
-import { useEffect, useState } from 'react'
+
 import BookList from '../components/BookList'
 import BookForm from '../components/BookForm'
-
-import { db } from '../firebase/config'
-import { collection, getDocs, onSnapshot } from 'firebase/firestore'
+import { useCollection } from '../hooks/useCollection';
 
 
 export default function Home() {
-  const [books, setBooks] = useState(null)
+  const {documents: books} = useCollection('books')
 
-  useEffect(() => {
-      const ref = collection(db, 'books')
-
-      getDocs(ref)
-      .then((snapshot)=> {
-
-        let results = []
-        snapshot.docs.forEach(doc => {
-          results.push({id: doc.id, ...doc.data()})
-        })
-
-        setBooks(results)
-      })
-
-  }, [])
 
   return (
     <div>
