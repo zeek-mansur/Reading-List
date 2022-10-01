270055168c6722124cd3ee61f2e064e85e3939f3
Zeek <mzulkiful@yahoo.com>
2022-10-01 12:18:57 +0100

    Getting Firestore Documents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import BookList from '../components/BookList'
 import BookForm from '../components/BookForm'
 
+import { db } from '../firebase/config'
+import { collection, getDocs, onSnapshot } from 'firebase/firestore'
+
+
 export default function Home() {
-  const [books, setBooks] = useState([
-    { title: 'the name of the wind', id: 1 },
-    { title: 'the dragon reborn', id: 2 },
-    { title: 'the final empire', id: 3 },
-    { title: 'the way of kings', id: 4 }
-  ])
+  const [books, setBooks] = useState(null)
+
+  useEffect(() => {
+      const ref = collection(db, 'books')
+
+      getDocs(ref)
+      .then((snapshot)=> {
+
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({id: doc.id, ...doc.data()})
+        })
+
+        setBooks(results)
+      })
+
+  }, [])
 
   return (
     <div>
